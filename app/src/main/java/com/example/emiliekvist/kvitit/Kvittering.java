25d0d37c7fd50b15a139ab59a1f97260eedbc72a
Madsmal <madsmalmskov@hotmail.com>
2017-06-19 12:31:27 +0200

    Mads er Spas

--- app/src/main/java/com/example/emiliekvist/kvitit/Kvittering.java
+++ app/src/main/java/com/example/emiliekvist/kvitit/Kvittering.java
@@ -15,6 +15,8 @@ public class Kvittering extends RealmObject {
     public String tags;
     public String photoPath;
 
+    public Kvittering() {}
+
     public Kvittering(Date dato, Date udløbsDato, String tags, String photoPath) {
         this.dato = dato;
         this.udløbsDato = udløbsDato;
